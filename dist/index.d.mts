69ea7f9ff801f3a46f26d85078b5d97dfaa52004
cinos81 <cinos81@gmail.com>
2023-08-12 16:18:41 +0900

    타입스크립트 개발환경 설정

---
+++ dist/index.d.mts
@@ -0,0 +1,4 @@
+declare const A = "hello";
+declare const B = "aaa";
+
+export { A, B };
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
     globalName: '_',
     entry: ['./index.ts'],
-    format: ['esm', 'cjs', 'iife']
+    format: ['esm', 'cjs', 'iife'],
+    dts: true,
 })
